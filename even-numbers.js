fa050cff6ba44516e427e8f5522f5eff435aa081
Nick <nicholas.mamino@student.swic.edu>
2024-03-26 18:52:30 -0500

    Implement for loop even numbers

---
+++ even-numbers.js
@@ -0,0 +1,9 @@
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numbers.length; i++) {
+    // This solution took me a rather long time to come too, 
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i]);
+    }
+}
