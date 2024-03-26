90cc1c90c69aeccb1908ce560bb843a7368b820b
Nick <nicholas.mamino@student.swic.edu>
2024-03-26 18:06:29 -0500

    While loop completion

--- fizzbuzz-while.js
+++ fizzbuzz-while.js
@@ -1,6 +1,14 @@
 let count = 0
 
 while (count <= 100){
-    console.log (count)
+    if (count % 3 === 0 && count % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (count % 3 === 0) {
+        console.log("Fizz");
+    } else if (count % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(count);
+    }
     count++
 }
