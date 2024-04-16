6f081c83bfe75bb29d2af6ddbf139b7a6bd3d6b6
Nick <nicholas.mamino@student.swic.edu>
2024-04-15 20:34:37 -0500

    Week 10 thursdayP1

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 // Create function 
-function greet(name){
-    return ("Hello, " + (name))
-}
+//function greet(name){
+    //return ("Hello, " + (name))
+//}
 
 //Call Function
- var greeting = greet("Nick")
+ //var greeting = greet("Nick")
 
 //Display
-console.log(greeting)
+//console.log(greeting)
+
+const person = {
+    name: "Nick",
+    age: 31,
+  };
+  
+  function verifyAdulthood(name, personAge) {
+    if (personAge >= 18) return `Welcome, ${name}!`;
+  
+    return `You are not old enough to enter, ${name}.`;
+  }
+  
+  const adultMessage = verifyAdulthood(person.name, person.age);
+  
+  console.log(adultMessage);
