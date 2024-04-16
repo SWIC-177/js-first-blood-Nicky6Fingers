d2e71d64605f148cd2af6cfebac7cfbad55b80a8
Nick <nicholas.mamino@student.swic.edu>
2024-04-15 20:50:44 -0500

    Week 10 thursday pt 2

--- index.js
+++ index.js
@@ -8,18 +8,54 @@
 
 //Display
 //console.log(greeting)
-
-const person = {
-    name: "Nick",
-    age: 31,
-  };
+//------------------------------------------------------------
+//const person = {
+  //  name: "Nick",
+    //age: 31,
+  //};
   
-  function verifyAdulthood(name, personAge) {
-    if (personAge >= 18) return `Welcome, ${name}!`;
+ // function verifyAdulthood(personName, personAge) {
+   // if (personAge >= 18) return `Welcome, ${personName}!`;
   
-    return `You are not old enough to enter, ${name}.`;
+  //  return `You are not old enough to enter, ${personName}.`;
+  //}
+  
+  //const adultMessage = verifyAdulthood(person.name, person.age);
+  
+  //console.log(adultMessage);
+  //---------------------------------------------------------------
+  const people = [
+    {
+      name: "John",
+      age: 17,
+    },
+    {
+      name: "Jane",
+      age: 20,
+    },
+    {
+      name: "Mary",
+      age: 25,
+    },
+    {
+      name: "Peter",
+      age: 30,
+    },
+    {
+      name: "Paul",
+      age: 35,
+    },
+  ];
+  
+  function verifyAdulthood(personName, personAge) {
+    if (personAge >= 18) return `Welcome, ${personName}!`;
+  
+    return `You are not old enough to enter, ${personName}.`;
   }
   
-  const adultMessage = verifyAdulthood(person.name, person.age);
-  
-  console.log(adultMessage);
+  // TODO: Use a loop to call the verifyAdulthood function for each person in the people array.
+
+  for (let i = 0; i < people.length; i++) {
+   const message = verifyAdulthood(people[i].name,people[i].age);
+   console.log(message)
+    }
